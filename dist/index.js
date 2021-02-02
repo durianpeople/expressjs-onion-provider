f15dbc747a8de51f2d7e6afc870d4cc37d47e869
Setya Wibawa <durianpeople@gmail.com>
2021-02-02 23:08:42 +0700

    remove dist

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Controller = void 0;
-const Controller_1 = require("./Controller");
-Object.defineProperty(exports, "Controller", { enumerable: true, get: function () { return Controller_1.Controller; } });
