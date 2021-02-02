14ffd1156ed3964fadc7fe4ac314979808bc31eb
Setya Wibawa <durianpeople@gmail.com>
2021-02-02 23:06:45 +0700

    init

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Controller = void 0;
+const Controller_1 = require("./Controller");
+Object.defineProperty(exports, "Controller", { enumerable: true, get: function () { return Controller_1.Controller; } });
